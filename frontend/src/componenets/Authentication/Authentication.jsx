2f33d81c7770bc1af828441913856bffb9d9dc00
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-04-07 03:03:50 +0300

    created signup with functionality

--- frontend/src/componenets/Authentication/Authentication.jsx
+++ frontend/src/componenets/Authentication/Authentication.jsx
@@ -4,12 +4,48 @@ import SignupForm from "./Forms/SignupForm";
 import SigninForm from "./Forms/SigninForm";
 
 function Authentication() {
-  const [isLogin, setIsLogin] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const register = async () => {
+    const formData = new FormData();
+    formData.append("name", signupData.name);
+    formData.append("email", signupData.email);
+    formData.append("password", signupData.password);
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/register", {
+        method: "POST",
+        body: JSON.stringify(signupData),
+      });
+      if (response.ok) {
+        console.log("User registed successfully");
+        console.log(response);
+      } else {
+        console.log("Registration failed");
+      }
+    } catch (error) {
+      console.log("Error occured during registration", "error");
+    }
+  };
+
   return (
     <div className="form-wrapper">
       <div className="form-container">
         <div className="instagram-logo">Instagram</div>
-        {isLogin ? <SigninForm /> : <SignupForm />}
+        {isLogin ? (
+          <SigninForm />
+        ) : (
+          <SignupForm
+            signupData={signupData}
+            setSignupData={setSignupData}
+            register={register}
+          />
+        )}
       </div>
     </div>
   );
--- frontend/src/componenets/Authentication/Forms/SignupForm.jsx
+++ frontend/src/componenets/Authentication/Forms/SignupForm.jsx
@@ -1,12 +1,46 @@
 import React from "react";
 
-function SignupForm() {
+function SignupForm({ signupData, setSignupData, register }) {
+  const handleSignup = () => {
+    register();
+  };
+
   return (
     <div className="signup-wrapper">
-      <input placeholder="Name"></input>
-      <input placeholder="Email"></input>
-      <input placeholder="Password"></input>
-      <input placeholder="Confirm Password"></input>
+      <input
+        placeholder="Name"
+        onChange={(e) => {
+          setSignupData({ ...signupData, name: e.target.value });
+        }}
+      ></input>
+      <input
+        placeholder="Email"
+        onChange={(e) => {
+          setSignupData({ ...signupData, email: e.target.value });
+        }}
+      ></input>
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => {
+          setSignupData({ ...signupData, password: e.target.value });
+        }}
+      ></input>
+      <input
+        placeholder="Confirm Password"
+        type="password"
+        onChange={(e) => {
+          setSignupData({ ...signupData, confirmPassword: e.target.value });
+        }}
+      ></input>
+
+      <div className="buttons-wrapper">
+        <button onClick={handleSignup}>Signup</button>
+      </div>
+
+      <div className="switcher">
+        <p>Already have an account? Signin</p>
+      </div>
     </div>
   );
 }
