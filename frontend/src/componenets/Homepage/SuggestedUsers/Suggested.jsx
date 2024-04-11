6d95fd51775aee12c7cc9ff1f095d1a67ee7b50a
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-04-11 15:01:51 +0300

    added toggleFollow function in suggested

--- frontend/src/componenets/Homepage/SuggestedUsers/Suggested.jsx
+++ frontend/src/componenets/Homepage/SuggestedUsers/Suggested.jsx
@@ -3,6 +3,31 @@ import "./Suggested.css";
 import userProfile from "../../../assets/profile-picture.jpeg";
 
 function Suggested() {
+  const toggleFollow = async (userId) => {
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/toggleFollow/${userId}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${JSON.parse(
+              localStorage.getItem("token")
+            )}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        console.log(`Error following user. Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log("Error toggling follow:", error.message);
+    }
+  };
+
   return (
     <div className="suggested-wrapper">
       <p className="suggested">Suggested for you</p>
